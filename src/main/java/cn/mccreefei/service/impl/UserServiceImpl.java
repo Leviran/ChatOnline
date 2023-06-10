9638daf1aed5485fccedd2eac184d6082efb92a8
Leviran <1447136316@qq.com>
2023-06-10 14:53:52 +0800

    add model & dao & service

---
+++ src/main/java/cn/mccreefei/service/impl/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package cn.mccreefei.service.impl;
+
+import cn.mccreefei.dao.LoginInfoDAO;
+import cn.mccreefei.dao.MessageRecordDAO;
+import cn.mccreefei.dao.UserDAO;
+import cn.mccreefei.model.LoginInfoDo;
+import cn.mccreefei.model.MessageRecordDo;
+import cn.mccreefei.model.User;
+import cn.mccreefei.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 用户service实现类
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDAO userDAO;
+    @Autowired
+    private LoginInfoDAO loginInfoDAO;
+    @Autowired
+    private MessageRecordDAO messageRecordDAO;
+
+
+    public User validateUserPassword(String name, String password) {
+        return userDAO.queryUser(name, password);
+    }
+
+    public boolean isExistUser(String name) {
+        User user = userDAO.queryUserByName(name);
+        return user != null;
+    }
+
+    public void insertUser(String name, String password) {
+        userDAO.insertUser(name, password);
+    }
+
+    @Override
+    public void addUserLoginInfo(LoginInfoDo loginInfoDo) {
+        loginInfoDAO.addLoginInfo(loginInfoDo);
+    }
+
+    @Override
+    public void addUserMessageRecord(MessageRecordDo messageRecordDo) {
+        messageRecordDAO.addMessageRecord(messageRecordDo);
+    }
+
+    @Override
+    public User getUserByName(String name) {
+        return userDAO.getUserByName(name);
+    }
+}
