4b2dd12e3786b344de00fc8c9f26975c511ecbd9
Leviran <1447136316@qq.com>
2023-06-10 15:31:07 +0800

    add login info

---
+++ src/main/java/cn/team/dao/LoginInfoDAO.java
@@ -0,0 +1,7 @@
+package cn.team.dao;
+
+import cn.team.model.LoginInfoDo;
+
+public interface LoginInfoDAO {
+    int addLoginInfo(LoginInfoDo loginInfoDo);
+}
--- src/main/java/cn/team/model/LoginInfoDo.java
+++ src/main/java/cn/team/model/LoginInfoDo.java
@@ -1,11 +1,6 @@
-package cn.mccreefei.model;
+package cn.team.model;
 
 import java.util.Date;
-
-/**
- * @author MccreeFei
- * @create 2018-04-28 下午1:32
- */
 public class LoginInfoDo {
     private Integer id;
     private Integer userId;
